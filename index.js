568d7a49cd85bd42be689bef125d24d2e62f02cd
TudbuT <tudbut@tudbut.de>
2022-02-27 23:00:42 +0100

    fix a counting bug in main page

--- index.js
+++ index.js
@@ -37,7 +37,9 @@ server.use(require('body-parser').urlencoded({extended: false}))
 server.all('/', function get(req, res) {
     const fake = req.body.fake === 'yes'
     let mainPage = {author: webname, title: 'All posts', content: 'These are all the posts on the board:', comments: []}
-    for (let i = 0; i < posts.length && i < 2000; i++) {
+    for (let i = 0; i < posts.length - 2000; i++) 
+        mainPage.comments.push(null)
+    for (let i = Math.max(0, posts.length - 2000); i < posts.length; i++) {
         mainPage.comments.push({timestamp: posts[i].timestamp, author: posts[i].author, title: posts[i].title, content: posts[i].content, comments: []}) 
     }
     if(req.body.name && req.body.title && req.body.content) {
